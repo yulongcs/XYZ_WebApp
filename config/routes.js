1634daf436edfeea4b99a8105e8b2e5066a08492
Endy Yu <yulongcs@qq.com>
2015-10-22 00:28:10 +0800

    change

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 ﻿var Index = require('../app/controlles/index.js');
-var ExchangePro = require('../app/controlles/exchangePro.js');
-var AdminProduct = require('../app/controlles/admin/product.js')
+var Exchange = require('../app/controlles/exchange.js');
+var AdminProduct = require('../app/controlles/admin/product.js');
+var Common = require('../app/controlles/_common.js');
 
 module.exports = function (app) {
 
@@ -8,8 +9,8 @@ module.exports = function (app) {
     app.get('/', Index.index)
 
     ////Exchange
-    app.get('/exchange/new', ExchangePro.new)
-    app.post('/exchange', ExchangePro.saveUploadImg, ExchangePro.save)
+    app.get('/exchange/new', Exchange.new)
+    app.post('/exchange', Exchange.save)
      
 
 
