9f2bcd198ede4d3216acb7d9c7336d25a26f5674
yulongcs <yulongcs@qq.com>
2015-10-23 18:19:00 +0800

    change

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,20 +1,30 @@
 ﻿
 $(function () {
+    $.metadata.setType("attr", "validate");
+    var validator = $("form").validate();
+
+    $("button[type=reset]").click(function() {
+        validator.resetForm();
+    });
+
     $('form').submit(function (ev) {
         ev.preventDefault();
-        var form = $(this);
-        $.ajax({
-            url: form.attr('action'),
-            type: 'POST',
-            data: form.serialize(),
-            success: function (obj) {
-                debugger;
-            }, error: function (jqXHR, textStatus, errorThrown) {
-                //textStatus :"timeout", "error", "notmodified" 和 "parsererror"。
-                var errorMsg = jqXHR.responseJSON.msg;
-                alert(errorMsg)
-            }
-        });
+        if (validator.form()) {
+            var form = $(this);
+            $.ajax({
+                url: form.attr('action'),
+                type: 'POST',
+                data: form.serialize(),
+                success: function(obj) {
+                    debugger;
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    //textStatus :"timeout", "error", "notmodified" 和 "parsererror"。
+                    var errorMsg = jqXHR.responseJSON.msg;
+                    alert(errorMsg)
+                }
+            });
+        }
     });
 
 });
